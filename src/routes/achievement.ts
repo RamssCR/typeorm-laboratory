328d6b1c25d26c16824d68177729d5ee022277d0
RamssCR <ramsesgpm@gmail.com>
2025-12-19 14:29:24 -0500

    feat: creado dominio de achievements

---
+++ src/routes/achievement.ts
@@ -0,0 +1,47 @@
+import {
+  createAchievement,
+  deleteAchievement,
+  getAchievementById,
+  getAchievements,
+  updateAchievement,
+} from '#controllers/achievement';
+import { AchievementService } from '#services/achievement';
+import { Router } from 'express';
+import { achievement } from '#schemas/achievement';
+import { container } from '#config/container';
+import { pagination } from '#schemas/pagination';
+import { params } from '#schemas/params';
+import { validate } from '#middlewares/schema';
+
+const achievementService =
+  container.resolve<AchievementService>(AchievementService);
+export const achievementRouter = Router();
+
+achievementRouter.get(
+  '/',
+  validate(pagination, { mode: 'partial', target: 'query' }),
+  validate(params.pick({ id: true }), { target: 'user' }),
+  getAchievements(achievementService),
+);
+achievementRouter.get(
+  '/:id',
+  validate(params.pick({ id: true }), { target: 'params' }),
+  getAchievementById(achievementService),
+);
+achievementRouter.post(
+  '/',
+  validate(achievement),
+  validate(params.pick({ id: true }), { target: 'user' }),
+  createAchievement(achievementService),
+);
+achievementRouter.patch(
+  '/:id',
+  validate(params.pick({ id: true }), { target: 'params' }),
+  validate(achievement, { mode: 'partial' }),
+  updateAchievement(achievementService),
+);
+achievementRouter.delete(
+  '/:id',
+  validate(params.pick({ id: true }), { target: 'params' }),
+  deleteAchievement(achievementService),
+);
