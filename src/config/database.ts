379ed884af052220e051049f5e4754b45b313306
RamssCR <ramsesgpm@gmail.com>
2025-12-19 14:27:07 -0500

    feat: agregado setter para el data source de typeorm

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,6 +8,7 @@ import {
   NODE_ENV,
 } from './environment.ts';
 import { DataSource } from 'typeorm';
+import { container } from './container.ts';
 import { logger } from '#utils/logger';
 
 export const appDataSource = new DataSource({
@@ -31,6 +32,7 @@ export const appDataSource = new DataSource({
 export const establishConnection = async () => {
   try {
     await appDataSource.initialize();
+    container.setDataSource(appDataSource);
     logger.info('Database connection established');
   } catch (error) {
     logger.error('Error establishing database connection:', error);
