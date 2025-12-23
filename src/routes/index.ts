fd1e23d84f6aff2efc3b35b4761932291d56e5aa
RamssCR <ramsesgpm@gmail.com>
2025-12-23 16:45:11 -0500

    feat: added middleware for token verification in /auth endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,11 @@ import { achievementRouter } from './achievement.ts';
 import { authRouter } from './auth.ts';
 import { tokenRouter } from './token.ts';
 import { userRouter } from './user.ts';
+import { verifyToken } from '#middlewares/verifyToken';
 
 export const router = Router();
 
 router.use('/users', userRouter);
 router.use('/achievements', achievementRouter);
 router.use('/tokens', tokenRouter);
-router.use('/auth', authRouter);
+router.use('/auth', verifyToken, authRouter);
