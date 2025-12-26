8fd638d34c5463d3f483cdec8a6f1dd430e2d99a
RamssCR <ramsesgpm@gmail.com>
2025-12-26 15:44:29 -0500

    fix: removed global middleware to assign into endpoint-specific routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,6 +4,7 @@ import { Router } from 'express';
 import { user } from '#schemas/user';
 import { container } from '#config/container';
 import { validate } from '#middlewares/schema';
+import { verifyToken } from '#middlewares/verifyToken';
 
 const authService = container.resolve<AuthService>(AuthService);
 export const authRouter = Router();
@@ -18,6 +19,6 @@ authRouter.post(
   validate(user.omit({ points: true })),
   register(authService),
 );
-authRouter.post('/profile', profile(authService));
-authRouter.post('/logout', logout(authService));
-authRouter.post('/refresh', refresh(authService));
+authRouter.post('/profile', verifyToken, profile(authService));
+authRouter.post('/logout', verifyToken, logout(authService));
+authRouter.post('/refresh', verifyToken, refresh(authService));
